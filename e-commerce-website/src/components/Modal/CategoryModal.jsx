f3e1704ea5cd416247793ff4fe9a73a104ca41ae
MoongJB-Dev <moongjbdev@gmail.com>
2024-12-15 21:02:19 +0700

    fillter UIUX

---
+++ e-commerce-website/src/components/Modal/CategoryModal.jsx
@@ -0,0 +1,104 @@
+/* eslint-disable react/prop-types */
+import CaretRight from "../../assets/Categorys/CaretRight.svg";
+import { useState, useEffect, useRef } from 'react';
+
+const categories = {
+  'Computer & Laptop': {
+    subCategories: ['Laptops', 'Desktop PCs', 'Gaming PCs', 'Monitors']
+  },
+  'SmartPhone': {
+    subCategories: ['Apple', 'Samsung', 'Xiaomi', 'Oppo', 'Vivo']
+  },
+  'Mobile Accessories': {
+    subCategories: ['Cases', 'Chargers', 'Screen Protectors', 'Power Banks']
+  },
+  'Gaming Console': {
+    subCategories: ['PlayStation', 'Xbox', 'Nintendo', 'Gaming Accessories']
+  },
+};
+
+const CategoryModal = ({ isOpen, onClose }) => {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const modalRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        // Kiểm tra xem click có phải là trên nút "All Category" không
+        const allCategoryButton = document.querySelector('[data-category-button]');
+        if (!allCategoryButton?.contains(event.target)) {
+          setSelectedCategory(null)
+          onClose();
+        }
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isOpen, onClose]);
+
+  const handleSelectedCategory = (fillter) => {
+    console.log("Category: ", JSON.stringify(fillter))
+    setSelectedCategory(null);
+    onClose();
+  };
+
+  if (!isOpen) return null;
+
+  return (
+    <div className="absolute left-0 bottom-0 translate-y-3 z-50">
+      <div ref={modalRef} className="absolute left-0 flex shadow-lg">
+        {/* Main Categories */}
+        <div className="relative">
+          <div className="w-64 bg-white py-3 border border-[#E4E7E9] rounded-sm">
+            {Object.keys(categories).map((category) => (
+              <div
+                key={category}
+                className={`px-4 py-3 cursor-pointer hover:bg-gray-100 group flex justify-between items-center 
+                ${selectedCategory === category ? 'after:content-[""]' : 'after:content-none'}
+                after:absolute after:right-0 after:top-0 after:h-full after:w-3
+                after:translate-x-full after:bg-transparent after:z-50
+                `}
+                onMouseEnter={() => setSelectedCategory(category)}
+                onClick={() => handleSelectedCategory({ category: category})}
+              >
+                <span className="text-[#5F6C72] group-hover:text-[#191C1F]">{category}</span>
+                {categories[category].subCategories && (
+                  <img
+                    src={CaretRight}
+                    alt="arrow-right"
+                    className="w-4 h-4 hidden group-hover:block"
+                  />
+                )}
+              </div>
+            ))}
+          </div>
+        </div>
+        {/* Sub Categories */}
+        {selectedCategory && (
+          <div className="absolute left-full translate-x-3 top-0 w-64 py-3 bg-white border border-[#E4E7E9] rounded-sm"
+            onMouseLeave={() => setSelectedCategory(null)}
+          >
+            {categories[selectedCategory].subCategories.map((subCategory) => (
+              <div
+                key={subCategory}
+                className="px-4 py-3 cursor-pointer hover:bg-gray-100 text-[#5F6C72] hover:text-[#191C1F]"
+                onClick={() => handleSelectedCategory({ category: selectedCategory, subCategorie: subCategory})}
+              >
+                {subCategory}
+              </div>
+            ))}
+          </div>
+        )}
+
+      </div>
+    </div>
+  );
+};
+
+export default CategoryModal
--- e-commerce-website/src/components/Navigation.jsx
+++ e-commerce-website/src/components/Navigation.jsx
@@ -1,46 +1,64 @@
 import ArrowDown from "../assets/Navigation/ArrowDown.svg";
+import ArrowUp from "../assets/Navigation/ArrowUp.svg";
+
 import Map from "../assets/Navigation/MapPinLine.svg";
 import Compare from "../assets/Navigation/Compare.svg";
 import Headphones from "../assets/Navigation/Headphones.svg";
 import Info from "../assets/Navigation/Info.svg";
 import PhoneCall from "../assets/Navigation/PhoneCall.svg";
 
-
+import CategoryModal from "./Modal/CategoryModal";
+import { useState } from "react";
 const Navigation = () => {
+  const [isCategoryModalOpen, setCategoryModalOpen] = useState(false);
   return (
     <div className="h-[81px] border-b border-[#E4E7E9]">
       <div className="max-w-main h-full m-auto py-4">
         <div className="flex items-center justify-between">
           {/* fillter */}
-          <div className="flex h-12">
-            <div className="flex w-[154px] py-[14px] px-6 bg-[#F2F4F5] items-center justify-between cursor-pointer">
+          <div className="flex h-12 relative">
+            <div
+              data-category-button
+              // onClick={() => setCategoryModalOpen(!isCategoryModalOpen)}
+              onMouseEnter={() => setCategoryModalOpen(true)}
+              onMouseLeave={() => setCategoryModalOpen(false)}
+              className={`relative flex w-[154px] py-[14px] px-6 ${isCategoryModalOpen ? 'bg-[#FA8232] text-white' : 'bg-[#F2F4F5] text-[#191C1F]'} 
+                items-center justify-between cursor-pointer
+                ${isCategoryModalOpen ? 'after:content-[""]' : 'after:content-none'}
+                after:h-3 after:w-full after:translate-y-3 
+                after:absolute after:left-0 after:bottom-0 
+                after:bg-transparent after:z-50 `}>
               <span className="font-medium text-sm mr-2">All Category</span>
-              <img src={ArrowDown} alt="arrow-down" className="w-4 h-4"/>
+              <img src={isCategoryModalOpen ? ArrowUp : ArrowDown} alt="arrow-down" className="w-4 h-4" />
+              <CategoryModal
+                isOpen={isCategoryModalOpen}
+                onClose={() => setCategoryModalOpen(false)}
+              />
             </div>
             {/* Map */}
             <div className="h-full py-[14px] ml-6 flex items-center justify-start cursor-pointer">
-              <img src={Map} alt="" className="w-6 h-6 mr-[6px]"/>
+              <img src={Map} alt="" className="w-6 h-6 mr-[6px]" />
               <span className="text-[#5F6C72] text-[14px] font-normal ">Track Order</span>
             </div>
             {/* Compare */}
             <div className="h-full py-[14px] ml-6 flex items-center justify-start cursor-pointer">
-              <img src={Compare} alt="" className="w-6 h-6 mr-[6px]"/>
+              <img src={Compare} alt="" className="w-6 h-6 mr-[6px]" />
               <span className="text-[#5F6C72] text-[14px] font-normal ">Compare</span>
             </div>
             {/* Customer support */}
             <div className="h-full py-[14px] ml-6 flex items-center justify-start cursor-pointer">
-              <img src={Headphones} alt="" className="w-6 h-6 mr-[6px]"/>
+              <img src={Headphones} alt="" className="w-6 h-6 mr-[6px]" />
               <span className="text-[#5F6C72] text-[14px] font-normal ">Customer Support</span>
             </div>
             {/* Help */}
             <div className="h-full py-[14px] ml-6 flex items-center justify-start cursor-pointer">
-              <img src={Info} alt="" className="w-6 h-6 mr-[6px]"/>
+              <img src={Info} alt="" className="w-6 h-6 mr-[6px]" />
               <span className="text-[#5F6C72] text-[14px] font-normal ">Need Help</span>
             </div>
           </div>
           {/* Phone number */}
           <div className="flex">
-            <img src={PhoneCall} alt="phone-call" className="w-7 h-7 mr-[2px]"/>
+            <img src={PhoneCall} alt="phone-call" className="w-7 h-7 mr-[2px]" />
             <span className="text-[18px] font-normal text-[#191C1F]">+84 396.979.999</span>
           </div>
         </div>
